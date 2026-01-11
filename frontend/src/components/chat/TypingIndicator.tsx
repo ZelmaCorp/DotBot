794757062b69ef2afd5519f59f9ccc41c7ab2540
Peter Ott (Valeska) <ottpeter@tutanota.io>
2026-01-11 16:41:26 +0100

    fixes TypingIndicator

--- frontend/src/components/chat/TypingIndicator.tsx
+++ frontend/src/components/chat/TypingIndicator.tsx
@@ -10,7 +10,6 @@ import React from 'react';
 const TypingIndicator: React.FC = () => {
   return (
     <div className="message bot">
-      <div className="message-avatar bot">D</div>
       <div className="message-content">
         <div className="typing-indicator">
           <div className="typing-dot"></div>
